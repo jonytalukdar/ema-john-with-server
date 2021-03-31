eab93969c9882f4920f8dd8e881caf3ad6b653b7
jonytalukdar <jonytalukdar30@gmail.com>
2021-03-31 19:18:26 +0600

    connect with batabse

--- ema-john-server/index.js
+++ ema-john-server/index.js
@@ -1,6 +1,29 @@
 const express = require('express');
+require('dotenv').config();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const MongoClient = require('mongodb').MongoClient;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.shsop.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
 const app = express();
 const port = 5000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+client.connect((err) => {
+  const products = client.db('emaJohnStore').collection('products');
+  app.post('addProducts', (req, res) => {
+    const product = req.body;
+    products.insertOne(product).then((result) => {
+      console.log(result);
+    });
+  });
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
